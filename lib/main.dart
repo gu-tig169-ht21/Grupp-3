4969fd423aec037a5f65e8fa410ef64a8aef15d8
naumburg <gusnaumhe@student.gu.se>
2021-12-09 13:28:26 +0100

    firebase html

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'views/homepageview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
-   MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: FutureBuilder(future: _fbApp, builder: (context, snapshot) {
+      home: FutureBuilder(builder: (context, snapshot) {
         if (snapshot.hasError) {
            print ('You have an error! ${snapshot.error.toString()}');
            return const Text('Something went wrong');
