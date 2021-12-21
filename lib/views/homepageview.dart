70d8315c2205550aa6bc1d93d34c162d175ef3c8
naumburg <gusnaumhe@student.gu.se>
2021-12-21 14:30:57 +0100

    helenaTimer

--- lib/views/homepageview.dart
+++ lib/views/homepageview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app/views/taskview.dart';
+import 'package:my_first_app/views/timerviews/timerview.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -20,7 +21,10 @@ class HomePage extends StatelessWidget {
             const Text('Pluggis-kompis'),
             Container(height: 20),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TimerView()));
+              },
               child: const Text('PLUGGA'),
             ),
             Container(height: 30),
