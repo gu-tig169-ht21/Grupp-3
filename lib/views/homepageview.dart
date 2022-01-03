cc9d728b485f16f409710429e887886f619a6df0
Hanuk0 <gushanhaj@student.gu.se>
2022-01-03 16:12:39 +0100

    Fix på homepage

--- lib/views/homepageview.dart
+++ lib/views/homepageview.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:my_first_app/views/taskview.dart';
 import 'timerviews/firsttimerview.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -11,24 +10,23 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('StudyBuddy'),
+        title: const Text('Smart Study'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(height: 40),
-            const Text('Pluggis-kompis'),
-            Container(height: 20),
+            _logo(),
+            Container(height: 30),
             ElevatedButton(
               onPressed: () {
-               Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => FirstTimerView())); 
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => FirstTimerView()));
               },
               child: const Text('PLUGGA'),
             ),
-            Container(height: 30),
+            Container(height: 25),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(context,
@@ -39,6 +37,29 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _logo() {
+    return Stack(
+      children: [
+        const Image(image: AssetImage('assets/logo.png')),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 200,
+            decoration: (BoxDecoration(
+              border: Border.all(
+                color: Colors.blueGrey,
+                width: 2,
+              ),
+              borderRadius: BorderRadius.circular(95),
+            )),
+          ),
+        ),
+      ],
     );
   }
 }
